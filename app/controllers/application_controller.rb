b2e2f09b81158c0f3eb130faa82c4a562f7fa389
Riccardo Cambiassi <r.cambiassi@gmail.com>
2012-12-08 23:43:21 +0000

    add basic auth. fixes #4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
+  http_basic_authenticate_with :name => ENV['EDITOR_USERNAME'], :password => ENV['EDITOR_PASSWORD']
   protect_from_forgery
 end
