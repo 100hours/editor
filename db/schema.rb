c3b39977c8a63cbbe058c15be2276706437494ce
Riccardo Cambiassi <r.cambiassi@gmail.com>
2012-12-12 21:54:03 +0000

    Add requester and word count field to Document

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121209222210) do
+ActiveRecord::Schema.define(:version => 20121212173615) do
 
   create_table "documents", :force => true do |t|
     t.string   "title"
     t.text     "body"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "requester"
+    t.integer  "word_count"
   end
 
   create_table "feedbacks", :force => true do |t|
