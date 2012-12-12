3bd738e387634c167ec094236091b5b4005ef91b
Riccardo Cambiassi <r.cambiassi@gmail.com>
2012-12-12 23:36:18 +0000

    david should input the requester email before starting writing

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -27,8 +27,7 @@ class DocumentsController < ApplicationController
   # GET /documents/new
   # GET /documents/new.json
   def new
-    document = Document.create
-    redirect_to edit_document_path document
+    @document = Document.new
   end
 
   # GET /documents/1/edit
@@ -43,7 +42,7 @@ class DocumentsController < ApplicationController
 
     respond_to do |format|
       if @document.save
-        format.html { redirect_to @document, notice: 'Document was successfully created.' }
+        format.html { redirect_to edit_document_path(@document), notice: 'Document was successfully created.' }
         format.json { render json: @document, status: :created, location: @document }
       else
         format.html { render action: "new" }
