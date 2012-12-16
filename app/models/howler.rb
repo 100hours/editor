d254734f59a49c01046a920caabda2554916eec0
Riccardo Cambiassi <r.cambiassi@gmail.com>
2012-12-16 23:59:39 +0000

    Add Howler model

---
+++ app/models/howler.rb
@@ -0,0 +1,22 @@
+class Howler < ActiveRecord::Base
+  after_initialize :default_values
+  before_validation :make_url_token, if: :new_record?
+  attr_accessible :charges, :notes
+
+  validates_presence_of :url, :charges
+  validates_uniqueness_of :url
+  validates_numericality_of :charges, greater_than_or_equal_to: 1
+
+  def played?
+    !played_at.nil?
+  end
+
+  private
+    def default_values
+      self.charges ||= 1
+    end
+
+    def make_url_token
+      self.url = SecureRandom.hex(32)
+    end
+end
